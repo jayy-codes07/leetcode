aef26b632e6c231a8d09f73ee28db8e51cf56cf6
hadiya jay <hadiyajay2010@gmail.com>
2026-03-30 18:31:38 +0000

    LeetCode: Runtime - 45 ms (72.46%), Memory - 53.4 MB (69.28%)

---
+++ 1013-fibonacci-number/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function (n) {
+
+  const arr = [0, 1]
+
+    for (let i = 2; i <= n; i++) {
+        arr.push(arr[i-1] + arr[i-2])    }
+
+    return arr[n]
+
+}
+console.log(fib(2));
