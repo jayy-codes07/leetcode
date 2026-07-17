8d7806d782d1019244e09f8aa3794fc4e06e2ef9
hadiya jay <hadiyajay2010@gmail.com>
2026-07-17 09:25:58 +0000

    LeetCode: Runtime - 0 ms (100.00%), Memory - 53.5 MB (74.82%)

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/solution.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/solution.js
@@ -4,20 +4,16 @@
  * @return {number}
  */
 var strStr = function (haystack, needle) {
-    let idx = -1
-    for (let i = 0; i < haystack.length; i++) {
-        let loop = 0
-
-        if (haystack[i] == needle[0]) {
-            for (let j = 0; j < needle.length; j++) {
-                if (haystack[i + j] !== needle[j]) {
-                    loop = 1
-                    break
-                }
+    for (let i = 0; i <= haystack.length - needle.length; i++) {
+        let match = true;
+        for (let j = 0; j < needle.length; j++) {
+            if (haystack[i + j] !== needle[j]) {
+                match = false;
+                break;
             }
-            if (loop == 0) return idx = i
         }
-
+        if (match) return i;
     }
-    return idx
+    return -1;
+
 };
