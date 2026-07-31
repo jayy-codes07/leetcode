eb3709287e4b8e5d5367e5a87e6d8e59e6f58361
hadiya jay <hadiyajay2010@gmail.com>
2026-07-31 06:12:19 +0000

    LeetCode: Runtime - 0 ms (100.00%), Memory - 58.1 MB (55.24%)

---
+++ 0792-binary-search/solution.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+    let first = 0
+    let last = nums.length - 1
+
+    while (first <= last) {
+        let middle = Math.floor((first + last) / 2)
+        if (nums[middle] == target) {
+            return middle
+        } else if (nums[middle] > target) {
+            last = middle - 1
+        } else {
+            first = middle + 1
+        }
+    }
+    return -1
+};
