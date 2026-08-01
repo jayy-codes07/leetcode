b5817792ce94916a4a69510e59a6b00ee252c4e0
hadiya jay <hadiyajay2010@gmail.com>
2026-08-01 15:01:54 +0000

    LeetCode: Runtime - 0 ms (100.00%), Memory - 57.6 MB (10.06%)

---
+++ 4058-compute-alternating-sum/solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var alternatingSum = function (nums) {
+    let result = 0
+    for (let i = 0; i < nums.length; i++) {
+        if (i % 2 == 0) { 
+            result += nums[i]
+        }else{
+            result -= nums[i]
+        }
+    }
+
+    return result
+};
