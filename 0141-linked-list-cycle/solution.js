33b9bb6e2699cd4a29c2bfc997274980d52fb184
hadiya jay <hadiyajay2010@gmail.com>
2026-09-10 04:31:52 +0000

    LeetCode: Runtime - 48 ms (84.13%), Memory - 60.2 MB (7.72%)

---
+++ 0141-linked-list-cycle/solution.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+    let visited = new Set()
+    while (head) {
+        if (visited.has(head)) return true
+
+        visited.add(head)
+        head = head.next
+    }
+
+    return false
+};
